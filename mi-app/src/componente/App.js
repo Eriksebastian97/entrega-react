a3647c8f6953c8bbb30443a72162402fed006704
erik godoy <sebastianerik802@gmail.com>
2022-07-20 23:17:33 -0300

    entrega

---
+++ mi-app/src/componente/App.js
@@ -0,0 +1,19 @@
+import Header from "./NavBar"
+import Main from "./main"
+function App(){
+    return (
+      <>
+     <Header />
+     
+     < Main / >
+      
+      <footer>
+        <p> footer</p>
+      </footer>
+
+      </>
+    )
+  
+  }
+
+export default App 
---
+++ mi-app/src/componente/NavBar.js
@@ -0,0 +1,14 @@
+function Header(){
+    return(
+        <header id="header"> 
+        <h1 className="titulo"> BIEMBENIDO A LA INTERNET SEÑOR CLIENTE XD</h1>
+        <nav>
+          <a href="#">calle falsa 123</a>  
+       <br />
+          <a href="#">messirve</a>
+        </nav>
+        </header>
+    )
+}
+
+export default Header ;
